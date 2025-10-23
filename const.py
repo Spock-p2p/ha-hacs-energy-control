f2e0cc5a738ffe0c74e79e14aa21a84356b5bd0e
Enric <164487968+spocker-1@users.noreply.github.com>
2025-10-23 15:37:22 +0200

    Delete const.py

--- const.py
+++
@@ -1,5 +0,0 @@
-DOMAIN = "energy_control"
-CONF_ENTITIES = "entities"
-UPDATE_INTERVAL = 15  # seconds
-ENDPOINT = "https://flex.spock.es/api/status"
-SWITCH_ENTITY_ID = "switch.spock_energy_control_active"
