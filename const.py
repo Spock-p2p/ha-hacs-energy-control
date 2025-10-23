e790f3e19d48384c010f49d279ec983ed956593d
Enric <164487968+spocker-1@users.noreply.github.com>
2025-10-23 10:01:09 +0200

    Create const.py

---
+++ const.py
@@ -0,0 +1,5 @@
+DOMAIN = "energy_control"
+CONF_ENTITIES = "entities"
+UPDATE_INTERVAL = 15  # seconds
+ENDPOINT = "https://flex.spock.es/api/status"
+SWITCH_ENTITY_ID = "switch.spock_energy_control_active"
