66048084f6e461d2d2449025f2688b20ba7a7c9d
Enric <164487968+spocker-1@users.noreply.github.com>
2025-11-04 16:22:53 +0100

    Update const.py

--- custom_components/spock_energy_control/const.py
+++ custom_components/spock_energy_control/const.py
@@ -5,6 +5,7 @@ DOMAIN = "spock_energy_control"
 
 # --- Config Flow ---
 CONF_API_TOKEN = "api_token"
+CONF_PLANT_ID = "plant_id" 
 CONF_GREEN_DEVICES = "green_devices"
 CONF_YELLOW_DEVICES = "yellow_devices"
 
@@ -12,7 +13,8 @@ CONF_YELLOW_DEVICES = "yellow_devices"
 DEFAULT_SCAN_INTERVAL_S = 60
 
 # --- Plataformas ---
-PLATFORMS: list[str] = ["sensor", "switch"]
+PLATFORMS: list[str] = ["sensor", "switch"] 
 
 # --- API ---
 HARDCODED_API_URL = "https://flex.spock.es/api/status"
+HARDCODED_API_URL_TELEMETRIA = "https://iot-ha.spock.es/api/iot_telemetry"
